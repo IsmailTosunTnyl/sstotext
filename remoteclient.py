8162057a54e1a78f7c0278d87bbb39f5187a4214
İsmail Tosun <ismailtosuntnyl@gmail.com>
2023-11-29 23:43:14 +0300

    added remote client

---
+++ remoteclient.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+import socket
+import json
+import pyperclip
+
+def send_json_data():
+    # Get the coordinates from the entry fields or use default values
+    x1_val = int(entry_x1.get() or 100)
+    y1_val = int(entry_y1.get() or 200)
+    x2_val = int(entry_x2.get() or 1800)
+    y2_val = int(entry_y2.get() or 800)
+    
+    # Get the IP address from the entry field or use default 'localhost'
+    ip_address = entry_ip.get() or 'localhost'
+
+    # Sample dictionary to send
+    data_to_send = {
+        'command': 'SS',
+        'coordinates': {'x1': x1_val, 'y1': y1_val, 'x2': x2_val, 'y2': y2_val}
+    }
+
+    # Convert dictionary to JSON bytes
+    json_data = json.dumps(data_to_send).encode()
+
+    try:
+        # Create a socket and connect to the server
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((ip_address, 12345))
+
+        # Send the JSON data
+        client_socket.sendall(json_data)
+
+        # Receive echoed data from the server
+        received_data = client_socket.recv(8192)
+        decoded_data = received_data.decode()
+
+        decoded_dict = json.loads(decoded_data)
+        print('Received from server:', decoded_dict)
+        pyperclip.copy(decoded_dict['text'])
+        # Close the socket
+        client_socket.close()
+    except ConnectionRefusedError:
+        print("Connection refused. Make sure the server is running.")
+
+# Create the tkinter window
+root = tk.Tk()
+root.title("Send JSON Data")
+
+# Entry field for IP address with default value 'localhost'
+label_ip = tk.Label(root, text="Server IP Address:")
+label_ip.pack()
+entry_ip = tk.Entry(root)
+entry_ip.insert(0, "localhost")  # Default value for IP address
+entry_ip.pack()
+
+# Entry fields for coordinates with default values
+label_x1 = tk.Label(root, text="X1:")
+label_x1.pack()
+entry_x1 = tk.Entry(root)
+entry_x1.insert(0, "100")  # Default value for x1
+entry_x1.pack()
+
+label_y1 = tk.Label(root, text="Y1:")
+label_y1.pack()
+entry_y1 = tk.Entry(root)
+entry_y1.insert(0, "200")  # Default value for y1
+entry_y1.pack()
+
+label_x2 = tk.Label(root, text="X2:")
+label_x2.pack()
+entry_x2 = tk.Entry(root)
+entry_x2.insert(0, "1800")  # Default value for x2
+entry_x2.pack()
+
+label_y2 = tk.Label(root, text="Y2:")
+label_y2.pack()
+entry_y2 = tk.Entry(root)
+entry_y2.insert(0, "800")  # Default value for y2
+entry_y2.pack()
+
+# Function to trigger when the button is clicked
+def button_click():
+    send_json_data()
+
+# Create a button in the window
+send_button = tk.Button(root, text="Get Data", command=button_click)
+send_button.pack()
+
+# Start the GUI
+root.mainloop()
--- textoimage.py
+++ textoimage.py
@@ -1,18 +1,52 @@
 import tkinter as tk
 import pyscreenshot
 import pytesseract
-from PIL import Image, ImageDraw
+from PIL import ImageDraw
 import pyperclip
+from threading import Thread
+
+import socket
+import json
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 12345))
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-def capture_and_ocr():
+# define a thread for remote control
+
+
+
+
+
+
+def capture_and_ocr(coordinates=None):
     try:
 
-        # Get the coordinates from the entry fields
-        x1 = int(entry_x1.get())
-        y1 = int(entry_y1.get())
-        x2 = int(entry_x2.get())
-        y2 = int(entry_y2.get())
+        if coordinates is not None:
+            # Get the coordinates from the entry fields
+            x1 = int(coordinates['x1'])
+            y1 = int(coordinates['y1'])
+            x2 = int(coordinates['x2'])
+            y2 = int(coordinates['y2'])
+            
+            # set the entry fields to the coordinates
+            entry_x1.delete(0, tk.END)
+            entry_x1.insert(0, x1)
+            entry_y1.delete(0, tk.END)
+            entry_y1.insert(0, y1)
+            entry_x2.delete(0, tk.END)
+            entry_x2.insert(0, x2)
+            entry_y2.delete(0, tk.END)
+            entry_y2.insert(0, y2)
+        else:
+            
+            
+            # Get the coordinates from the entry fields
+            x1 = int(entry_x1.get())
+            y1 = int(entry_y1.get())
+            x2 = int(entry_x2.get())
+            y2 = int(entry_y2.get())
         
         print("Capturing screenshot from (" + str(x1) + ", " + str(y1) + ") to (" + str(x2) + ", " + str(y2) + ")")
         # Capture the screenshot based on entered coordinates
@@ -26,6 +60,7 @@ def capture_and_ocr():
         # Display OCR result or do further processing
         print("OCR Result:")
         print(text)
+        return text
     except ValueError:
         print("Please enter valid integer values for coordinates.")
 
@@ -48,6 +83,28 @@ def show_area():
 
     # Display the modified screenshot
     screenshot.show()
+
+
+
+def remote_control():
+    server_socket.listen(5)
+    while True:
+        client_socket, address = server_socket.accept()
+        print(f"Connection from {address} has been established!")
+        received_data = client_socket.recv(8192 )
+        decoded_data = received_data.decode()
+
+        # Convert the received JSON data to a dictionary
+        received_dict = json.loads(decoded_data)
+        print('Received from client:', received_dict)
+
+        received_dict['status'] = 'success'
+        received_dict['text'] = capture_and_ocr(received_dict['coordinates'])
+        # Send the received data back to the client
+        client_socket.sendall(json.dumps(received_dict).encode())       
+        # Close the client socket after sending data back
+        client_socket.close()
+
 
 # Create a tkinter window set its title and size
 
@@ -88,6 +145,11 @@ capture_button.pack()
 
 clear_button = tk.Button(root, text="Show Area", command=show_area)
 clear_button.pack()
-
+t1 = Thread(target=remote_control)
+t1.start()
 # Start the GUI
 root.mainloop()
+
+# Close the socket
+server_socket.close()
+t1.join()
